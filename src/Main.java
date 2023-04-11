83b88fe4374df665e23703363f324639994df4d3
alba-salesianos <usr3596@salesianos-lacuesta.net>
2023-04-11 10:36:07 +0100

    update: created generic Pokemon class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import Types.Pokemon;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pokemon pikachu = new Pokemon("Pikachu","Electric",1.04,13.2,"When it is angered, it immediately discharges the energy stored in the pouches in its cheeks.");
+
+        System.out.println(pikachu);
     }
 }
--- src/Types/Pokemon.java
+++ src/Types/Pokemon.java
@@ -1,4 +1,68 @@
 package Types;
 
 public class Pokemon {
+    protected String name;
+    protected String type;
+    protected double height;
+    protected double weight;
+    protected String description;
+    public Pokemon(String name, String type, double height, double weight, String description){
+        this.name = name;
+        this.type = type;
+        this.height = height;
+        this.weight = weight;
+        this.description = description;
+    }
+
+    public Pokemon(String name, String type){
+        this.name = name;
+        this.type = type;
+        this.height = height;
+        this.weight = weight;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        String msg = "════════════════════════════════════";
+        msg += "\n" + this.name.toUpperCase();
+        msg += "\n" + this.type.toUpperCase();
+        msg += "\n" + "HT    " + this.height;
+        msg += "\n" + "WT    " + this.weight + "lb";
+        msg += "\n" + "──█──█──█──█────────────█──█──█──█──";
+        msg += "\n" + this.description;
+        return msg;
+    }
 }
