b830644f2b1f024f0349bc454a380833aba568ca
alba-salesianos <usr3596@salesianos-lacuesta.net>
2023-04-18 08:42:20 +0100

    feat: added methods for adding and deleting pokemon

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
 import Types.Legendary;
 import Types.Pokemon;
+import utilities.ListaPokemon;
 
 public class Main {
     public static void main(String[] args) {
         Pokemon pikachu = new Pokemon("Pikachu","Electric","1'04\"",13.2,"When it is angered, it immediately discharges the energy stored in the pouches in its cheeks.");
 
-        System.out.println(pikachu);
-
         Legendary articuno = new Legendary("Articuno", "Freeze", "5'07\"", 122.1, "It’s said that this Pokémon’s beautiful blue wings are made of ice. Articuno flies over snowy mountains, its long tail fluttering along behind it.", "Seafoam Islands");
 
-        System.out.println(articuno);
+        Pokemon nintetales = new Pokemon("Ninetales","Fire","3'07\"",43.9,"It is said to live 1,000 years, and each of its tails is loaded with supernatural powers.");
+
+
+        ListaPokemon pokemones = new ListaPokemon();
+
+        pokemones.addPokemon(pikachu);
+        pokemones.addPokemon(articuno);
+        pokemones.addPokemon(nintetales);
+
+        System.out.println(pokemones);
+
+        pokemones.deletePokemon("Ditto");
+
+        System.out.println(pokemones);
+
+
+
 
     }
 
---
+++ src/utilities/ListaPokemon.java
@@ -0,0 +1,73 @@
+package utilities;
+
+import Types.Pokemon;
+
+public class ListaPokemon {
+    //private int listSize = 1;
+    private int listSize = 0;
+    private Pokemon pokemonList[] = new Pokemon[1];
+
+    public ListaPokemon(){
+        this.pokemonList = pokemonList;
+    }
+
+    public void addPokemon(Pokemon newPokemon){
+        pokemonList[listSize]= newPokemon;
+        listSize++;
+
+        Pokemon tempPokemonList[] = new Pokemon[listSize + 1];
+
+        for (int i = 0; i < pokemonList.length; i++) {
+            tempPokemonList[i] = pokemonList[i];
+        }
+
+        this.pokemonList = tempPokemonList;
+    }
+
+    public void deletePokemon(String pokemonName){
+        boolean isDeleted = false;
+        int registeredPokemon = 0;
+        int counter = 0;
+        for (int i = 0; i < pokemonList.length-1; i++) {
+            if(pokemonList[i].getName().equals(pokemonName)){
+                pokemonList[i] = null;
+                isDeleted = true;
+            } else {
+                registeredPokemon++;
+
+            }
+        }
+
+        Pokemon tempPokemonList[] = new Pokemon[registeredPokemon+1];
+
+
+        for (int j = 0; j < pokemonList.length; j++) {
+
+            if(pokemonList[j]!=null){
+                tempPokemonList[counter] = pokemonList[j];
+                counter++;
+            }
+
+        }
+
+        this.pokemonList = tempPokemonList;
+
+        if(isDeleted == true) {
+            System.out.println("Pokemon eliminado.");
+        } else {
+            System.out.println("Este pokemon no se encuentra en la lista.");
+        }
+
+
+
+    }
+
+    @Override
+    public String toString() {
+        String msg = "";
+        for (int i = 0; i < pokemonList.length; i++) {
+            msg += "\n" + pokemonList[i];
+        }
+        return msg;
+    }
+}
